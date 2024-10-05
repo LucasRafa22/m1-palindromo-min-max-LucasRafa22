fd0542cd1d9cfdbc89c7db6c4e96480b3d92cb38
Lucas <lucassolimene22@gmail.com>
2024-10-05 20:36:45 -0300

    Terminado

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    let frase = ""
+    const strLimpo = str.replace(/\s+/g, '').toLowerCase()
+    for (i = strLimpo.length - 1; i >= 0; i--){
+        frase += strLimpo[i]
+    }
+    if(frase === strLimpo){
+        return true
+    }
+    return false
 }
+
+
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
