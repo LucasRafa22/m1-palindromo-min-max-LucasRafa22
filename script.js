4503073ffb02203e3ae318ec39b50353c7b0be1b
Lucas <lucassolimene22@gmail.com>
2024-10-05 21:18:22 -0300

    Agora sim

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function isPalindrome(str){
-    let frase = ""
+    let frase = "";
     const strLimpo = str.replace(/\s+/g, '').toLowerCase()
     for (i = strLimpo.length - 1; i >= 0; i--){
-        frase += strLimpo[i]
+        frase += strLimpo[i];
     }
     if(frase === strLimpo){
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 
 
 
 function arrayMaxMin(arr){
-    let minumo = 0
-    let maximo = 0
+    let minumo = 0;
+    let maximo = 0;
 
     for (i = 0; i < arr.length; i++){
         if(i === 0 || arr[i] < minumo){
-            minumo = arr[i]
+            minumo = arr[i];
         }
         if(arr[i] > maximo){
-            maximo = arr[i]
+            maximo = arr[i];
         }
     }
-    return [minumo, maximo]
+    return [minumo, maximo];
 
 }
